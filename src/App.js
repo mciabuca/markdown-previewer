d1b4465d3aab56ca5511c7ad4eb23f184e0ae44a
Michael Ciabuca <other404@pm.me>
2024-01-11 01:35:21 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { marked } from 'marked';
 
-function App() {
+const App = () => {
+  const [markdown, setMarkdown] = useState(initialMarkdown);
+
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
+  const getMarkdownText = () => {
+    var rawMarkup = marked.parse(markdown, {sanitize: true, breaks: true});
+    return { __html: rawMarkup };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Editor markdown={markdown} onChange={handleChange} />
+      <Preview markdown={getMarkdownText()} />
     </div>
   );
+};
+
+const Editor = ({ markdown, onChange }) => (
+  <textarea id="editor" value={markdown} onChange={onChange} />
+);
+
+const Preview = ({ markdown }) => (
+  <div id="preview" dangerouslySetInnerHTML={markdown} />
+);
+
+const initialMarkdown = `
+# Welcome to My Markdown Previewer!
+
+## This is a Subheading
+
+Here's a link to [freeCodeCamp](https://www.freecodecamp.org).
+
+Inline code can be written within backticks, like this: \`<div></div>\`.
+
+\`\`\`
+// This is a code block:
+function exampleFunction() {
+  console.log('Hello, World!');
 }
+\`\`\`
+
+- This is a list item
+
+> This is a blockquote.
+
+![Image](https://via.placeholder.com/150 "Placeholder Image")
+
+**This is bold text!**
+
+*This is italic text!*
+
+You can **_combine_** them.
+`;
 
 export default App;
